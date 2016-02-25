2b58ee09aaa1f61b943d79d25d60c334df80abbf
Brad Morse <bkmorse@gmail.com>
2016-02-25 12:37:19 -0500

    initial

---
+++ subsettings/README.md
@@ -0,0 +1,9 @@
+Sub Settings
+------------------
+
+This displays the settings in the left menu within the Craft Control Panel.
+
+This saves you a click. I missed the drop-down that displayed when you hovered over
+the Settings in the top right when the links existed there.
+
+Just install the plugin and it will display the links on the left control panel menu.
---
+++ subsettings/SubSettingsPlugin.php
@@ -0,0 +1,80 @@
+<?php
+namespace Craft;
+
+class SubSettingsPlugin extends BasePlugin
+{
+    // =========================================================================
+    // PLUGIN INFO
+    // =========================================================================
+
+    public function getName()
+    {
+        return Craft::t('CP Sub Settings');
+    }
+
+    public function getVersion()
+    {
+        return '0.0.1';
+    }
+
+    public function getSchemaVersion()
+    {
+        return '0.0.1';
+    }
+
+    public function getDeveloper()
+    {
+        return 'Brad Morse';
+    }
+
+    public function getDeveloperUrl()
+    {
+        return 'http://bkmorse.com';
+    }
+
+    public function getPluginUrl()
+    {
+        return '';
+    }
+
+    public function getDocumentationUrl()
+    {
+        // return $this->getPluginUrl() . '/blob/master/README.md';
+        return '';
+    }
+
+    public function getReleaseFeedUrl()
+    {
+        return '';
+    }
+
+    public function onBeforeInstall()
+    {   
+        // Craft 2.3.2615 getFieldsForElementsQuery()
+        if (version_compare(craft()->getVersion() . '.' . craft()->getBuild(), '2.3.2615', '<')) {
+            throw new Exception($this->getName() . ' requires Craft CMS 2.3.2615+ in order to run.');
+        }
+    }
+
+
+    // =========================================================================
+    // HOOKS
+    // =========================================================================
+
+    public function modifyCpNav(&$nav)
+    {
+        if (!craft()->userSession->isAdmin())
+            return false;
+
+        $cpvariable = new CpVariable;
+        $category_settings = $cpvariable->settings();
+
+        foreach($category_settings as $settings)
+        {
+            foreach($settings as $key => $value)
+            {
+                $nav[$value['icon']] = array('label' => $value['label'], 'url' => 'settings/'.$key);
+            }
+        }
+    }    
+}
